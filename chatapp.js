f409482ec14653674f020791aba6a2b1580668f2
James OToole <jtotoole@gmail.com>
2014-12-12 12:19:23 -0500

    basic working app

--- chatapp.js
+++ chatapp.js
@@ -1,1 +1,30 @@
-var ws = new WebSocket("ws://localhost:3000");
+var ws = new WebSocket("ws://james.princesspeach.nyc:3000");
+
+ws.addEventListener("message", function(evt){
+  var body = document.querySelector("body");
+  var newP = document.createElement("p");
+  newP.innerText = evt.data;
+  body.appendChild(newP);
+});
+
+var message = document.getElementById("input");
+var namebox = document.getElementById("namebox");
+var button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+  var hash = {name: namebox.value}
+  hash["words"] = message.value;
+  var stuff = JSON.stringify(hash);
+  ws.send(stuff);
+  message.value = "";
+});
+
+message.addEventListener("keypress", function(){
+  if (event.keyCode === 13){
+    var hash = {name: namebox.value}
+    hash["words"] = (message.value).toString().trim();
+    var stuff = JSON.stringify(hash);
+    ws.send(stuff);
+    message.value = "";
+  };
+});
--- chatappServer.js
+++ chatappServer.js
@@ -1,2 +1,25 @@
 var WebSocketServer = require("ws").Server;
 var server = new WebSocketServer({port: 3000});
+var history = [];
+var clients = [];
+
+server.on("connection", function(ws) {
+  console.log("Client connected!"); //lets server know a client has connected
+  clients.push(ws); //adds user to the list of people connected
+  ws.on("close", function (){
+    var x = clients.indexOf(ws);
+    clients.splice(x, 1);   //when user disconnects, removes user from user list
+    console.log(clients.length); //lets server know # of users in chatroom
+  });
+  var historyString = (history.join("\n"));
+  ws.send(historyString);   //sends newly connected user chat history
+  ws.on("message", function(x){
+    var hash = JSON.parse(x);
+    var hashDecoded = (hash.name + ": " + hash.words); //de-JSON client message
+    console.log(hashDecoded);
+    history.push(hashDecoded); // add message from client to chat history
+    clients.forEach(function(client){
+      client.send(hashDecoded); //send client message to all users in chat
+    });
+  });
+});
